77af0df144c8264ded4da03cbc4d95f25e362d8b
TheTubbyRaider <grsraider66@gmail.com>
2024-07-19 18:19:59 -0600

    Week 6 code

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -10,27 +10,48 @@ public class Node {
     public void Insert(int value) {
         if (value < Data) {
             // Insert to the left
-            if (Left is null)
+            if (Left is null) {
                 Left = new Node(value);
-            else
+            } else {
                 Left.Insert(value);
-        }
-        else {
+            }
+        } else if (value > Data) {
             // Insert to the right
-            if (Right is null)
+            if (Right is null) {
                 Right = new Node(value);
-            else
+            } else {
                 Right.Insert(value);
+            }
         }
+        // If value == Data, do nothing (no duplicates allowed)
     }
 
     public bool Contains(int value) {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data) {
+            return true;
+        } else if (value < Data) {
+            if (Left == null) {
+                return false;
+            } else {
+                return Left.Contains(value);
+            }
+        } else {
+            if (Right == null) {
+                return false;
+            } else {
+                return Right.Contains(value);
+            }
+        }
     }
 
     public int GetHeight() {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (Left == null && Right == null) {
+            return 1;
+        }
+        
+        int leftHeight = Left != null ? Left.GetHeight() : 0;
+        int rightHeight = Right != null ? Right.GetHeight() : 0;
+        
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
